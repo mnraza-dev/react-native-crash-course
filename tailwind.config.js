26c7a0a09de345baec915ee8033e01e42658c21c
MN Raza <mnraza94@gmail.com>
2024-12-07 19:59:14 +0530

    fixing issue of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("nativewind/tailwind")],
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
